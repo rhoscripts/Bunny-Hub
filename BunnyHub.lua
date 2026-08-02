b87082e0cf059d9bb04e19efe183140d17a114de
Rho <justxdiv@gmail.com>
2026-08-02 23:54:59 +0800

    Update script loader for BunnyHub.lua

--- BunnyHub.lua
+++ BunnyHub.lua
@@ -1,4 +1,4 @@
-if game.GameId ~= 2753915549 then return end
+-- Loader
 if not game:IsLoaded() then game.Loaded:Wait() end
 getgenv().SCRIPT_KEY = "0abad4ed-8db2-4aec-b16a-3b049f713d9d"
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/67736adfab8428a3046e86f6f0f22bf290e93793e28064f4d5490f7aed0dd8e0/download"))()
